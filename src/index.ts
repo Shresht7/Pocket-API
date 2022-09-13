3badd3537262227c0a0bce8e0834b74f48d46176
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-09-13 18:46:43 +0530

    Create Pocket Client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,97 @@
-console.log('Hello World')
+//  Library
+import { Request } from './utils/request'
+
+const request = new Request({
+    defaultOptions: {
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'X-Accept': 'application/json'
+        }
+    }
+})
+
+//  ==========
+//  POCKET API
+//  ==========
+
+const ENDPOINT = {
+    REQUEST_TOKEN: 'https://getpocket.com/v3/oauth/request',
+    OAUTH: 'https://getpocket.com/v3/oauth/authorize',
+    AUTHORIZE: 'https://getpocket.com/auth/authorize',
+    ADD: 'https://getpocket.com/v3/add',
+    RETRIEVE: 'https://getpocket.com/v3/get',
+}
+
+interface APIRequestOptions {
+    consumer_key: string
+}
+
+export class PocketClient {
+
+    private consumer_key: string
+
+    private redirect_uri: string = 'http://localhost:3000'
+
+    private request_token: string | undefined
+
+    private access_token: string | undefined
+
+    constructor(options: APIRequestOptions) {
+        this.consumer_key = options.consumer_key
+    }
+
+    async getRequestToken() {
+        if (this.request_token) { return this.request_token }
+
+        try {
+            const response = await request.post(ENDPOINT.REQUEST_TOKEN, {
+                consumer_key: this.consumer_key,
+                redirect_uri: this.redirect_uri,
+            }).then<{ code: string }>(res => res.json())
+            this.request_token = response.code
+        } catch (err) {
+            throw err
+        }
+
+        const authURL = `${ENDPOINT.AUTHORIZE}?${new URLSearchParams({ request_token: this.request_token, redirect_uri: this.redirect_uri }).toString()}`
+        console.log('Authorize at', authURL)
+
+        return this.request_token
+    }
+
+    setRequestToken(token: string) {
+        this.request_token = token
+        return this
+    }
+
+    async getAccessToken() {
+        if (this.access_token) { return this.access_token }
+
+        try {
+            const response = await request.post(ENDPOINT.OAUTH, {
+                consumer_key: this.consumer_key,
+                code: this.request_token,
+            }).then<{ access_token: string }>(res => res.json())
+            this.access_token = response.access_token
+        } catch (err) {
+            throw err
+        }
+
+        return this.access_token
+    }
+
+    setAccessToken(token: string) {
+        this.access_token = token
+        return this
+    }
+
+    add(url: string, details?: { title?: string, tags?: string, tweet_id?: string }) {
+        return request.post(ENDPOINT.ADD, {
+            url,
+            ...details,
+            consumer_key: this.consumer_key,
+            access_token: this.access_token
+        })
+    }
+
+}
