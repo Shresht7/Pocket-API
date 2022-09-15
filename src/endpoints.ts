62edbf0687ded167f8cb28276ca9832bdf3fd346
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-09-15 15:01:24 +0530

    Refactor: baseURL

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -2,11 +2,13 @@
 //  ENDPOINTS
 //  =========
 
+const baseURL = 'https://getpocket.com'
+
 export const ENDPOINT = {
-    REQUEST_TOKEN: 'https://getpocket.com/v3/oauth/request',
-    OAUTH: 'https://getpocket.com/v3/oauth/authorize',
-    AUTHORIZE: 'https://getpocket.com/auth/authorize',
-    ADD: 'https://getpocket.com/v3/add',
-    MODIFY: 'https://getpocket.com/v3/send',
-    RETRIEVE: 'https://getpocket.com/v3/get',
+    REQUEST_TOKEN: baseURL + '/v3/oauth/request',
+    OAUTH: baseURL + '/v3/oauth/authorize',
+    AUTHORIZE: baseURL + '/auth/authorize',
+    ADD: baseURL + '/v3/add',
+    MODIFY: baseURL + '/v3/send',
+    RETRIEVE: baseURL + '/v3/get',
 } as const
